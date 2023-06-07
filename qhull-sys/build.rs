f50fc15eabd851a32bc4a8dc2931a44192428c73
April Wade <wayedt@gmail.com>
2023-06-07 02:35:14 -0500

    Update qhull-sys build script for WASM

--- qhull-sys/build.rs
+++ qhull-sys/build.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 const C_FILES: &[&str] = &[
     "qhull/src/libqhull_r/global_r.c",
     "qhull/src/libqhull_r/stat_r.c",
@@ -17,6 +19,21 @@ const C_FILES: &[&str] = &[
     "src/ext.c",
 ];
 
+// These are header files we want to watch for changes too
+const HEADER_FILES: &[&str] = &[
+    "libqhull_r.h",
+    "qhull_ra.h",
+    "stat_r.h",
+    "user_r.h",
+    "mem_r.h",
+    "qset_r.h",
+    "random_r.h",
+    "io_r.h",
+    "merge_r.h",
+    "poly_r.h",
+    "geom_r.h",
+];
+
 fn main() {
     let mut build = cc::Build::new();
     build.include("qhull/src/")
@@ -24,8 +41,16 @@ fn main() {
         .pic(true)
         .flag("-ansi")
         .flag("-Wno-unused-but-set-variable");
+    if env::var("CARGO_CFG_TARGET_ARCH").as_deref() == Ok("wasm32") {
+        build.flag("-DWASM").flag("-flto");
+        build.flag(&format!("--sysroot={}", env::var("WASM_SYSROOT").unwrap()));
+        println!("cargo:rerun-if-env-changed=WASM_SYSROOT");
+    }
     for c_file in C_FILES {
         build.file(c_file);
+    }
+    for header_file in HEADER_FILES {
+        println!("cargo:rerun-if-changed={}", header_file);
     }
     build.compile("qhull_r");
 }
